966d5a80c0548445e42a014bc5a0de57964fefff
ayush08negi <negi08ayush@gmail.com>
2025-04-02 00:26:33 +0530

    changes added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
 import { Eye, EyeOff, Loader2, Mail, MessageSquare, User,Lock } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import AuthImagePattern from '../components/AuthimagePattern';
+import AuthImagePattern from '../components/AuthImagePattern';
 
 const Login = () => {
   const [showPassword,setShowPassword] = useState(false);
