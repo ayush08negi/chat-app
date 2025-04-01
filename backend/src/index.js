9db79ba0303016f97678d350309fc101b8a8ddee
ayush08negi <negi08ayush@gmail.com>
2025-04-02 00:18:11 +0530

    changes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import path from "path";
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import { connectDB } from './lib/db.js';
-import cors from 'cors'
 import { app,server } from './lib/Socket.js'
 
 dotenv.config();
+
 const PORT = process.env.PORT
-
-
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' })); 
@@ -25,6 +27,14 @@ app.use(
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages", messageRoutes )
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 server.listen(PORT, ()=>{
